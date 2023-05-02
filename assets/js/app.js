cf6a152f57c09070c8fa4f93916935ba5dc562d2
Angelika Czerwińska <angelikaczerwinska9@gmail.com>
2023-05-02 11:29:52 +0200

    More styling fixes

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+tns({
+  container: ".slider__element",
+  items: 5,
+  slideBy: "page",
+  autoplay: true,
+  controls: false,
+  autoplayButtonOutput: false,
+  navPosition: "bottom",
+  mouseDrag: true,
+});
